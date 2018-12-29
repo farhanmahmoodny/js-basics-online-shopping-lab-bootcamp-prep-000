0a8d8055861e0d09b63d075d2f499738a2459989
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-29 21:41:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ function placeOrder(cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
   else{
-  console.log('Your total cost is $' + total() + ', which will be charged to the card ' + cardNumber + '.')
+  cart.splice(0, (cart.length))
+    return 'Your total cost is $' + total() + ', which will be charged to the card ' + cardNumber + '.'
     }
   return cart.splice(0, (cart.length))
 }
