7be4f8ef1e09b0ff32bfc2bd661fee7a64646179
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-29 21:02:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,6 +62,5 @@ function placeOrder(cardNumber) {
   else{
     return 'Your total cost is $' + total() + ', which will be charged to the card ' + cardNumber + '.'
     }
-    cart.itemName.splice(0, cart.length)
-  return cart
+  return cart.splice(0, cart.length)
 }
